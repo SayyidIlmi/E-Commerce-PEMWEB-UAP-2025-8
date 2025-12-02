c35a51918834891ec237642561c68cf7f9752f12
SayyidIlmi <sayyidilmihubballillah@gmail.com>
2025-12-02 16:28:46 +0700

    merubah model user,ProductCategory,seeder user

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,8 +21,10 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
         'role',
+        'profile_picture',
+        'phone_number',
+        'password',
     ];
 
     /**
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+namespace Database\Seeders;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin123'),
+            'role'=> 'admin'
+        ]);
+
+        User::create(      [
+            'name' => 'Sega Corp',
+            'email' => 'sega@gmail.com',
+            'password' => Hash::make('sega123'),
+            'role'=> 'member'
+        ]
+        );
+        User::create([
+            'name' => 'Galaxy Solusindo',
+            'email' => 'gasol@gmail.com',
+            'password' => Hash::make('gasol123'),
+            'role'=> 'seller'
+        ]
+        );
+    }
+}
